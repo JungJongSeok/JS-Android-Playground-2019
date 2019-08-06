ea4893a2fdc21291dfb90df79ff43f8f7c044111
Jung Jong-Seok <ee5188@naver.com>
2019-08-06 16:14:17 +0900

    Add LiveData (#7)

--- app/src/main/java/com/js/playground/extension/Extension.kt
+++ app/src/main/java/com/js/playground/extension/Extension.kt
@@ -8,22 +8,22 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import kotlin.reflect.KClass
 
-fun <T : ViewModel> FragmentActivity?.viewModel(clazz: KClass<out T>): T {
+fun <T : ViewModel> FragmentActivity?.initViewModel(clazz: KClass<out T>): T {
     return this?.run {
         ViewModelProviders.of(this)[clazz.java]
     } ?: throw Exception("Invalid Activity")
 }
 
-fun <T : ViewModel> Fragment?.viewModel(clazz: KClass<out T>): T {
+fun <T : ViewModel> Fragment?.initViewModel(clazz: KClass<out T>): T {
     return this?.run {
         ViewModelProviders.of(this)[clazz.java]
     } ?: throw Exception("Invalid Fragment")
 }
 
-fun FragmentActivity.requestManager(): RequestManager {
+fun FragmentActivity.initRequestManager(): RequestManager {
     return Glide.with(this)
 }
 
-fun Fragment.requestManager(): RequestManager {
+fun Fragment.initRequestManager(): RequestManager {
     return Glide.with(this)
 }
